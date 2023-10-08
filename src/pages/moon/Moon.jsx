0337c0899ab388db7a716895d8d99155eff218e7
VFdo <vidufernando98@gmail.com>
2023-10-08 22:49:41 +0530

    moon completed

--- src/pages/moon/Moon.jsx
+++ src/pages/moon/Moon.jsx
@@ -1,31 +1,43 @@
+import React, { useState, useEffect } from "react";
 import '../pages.css'
 import Paper from '@mui/material/Paper';
 import { Box } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
 import { Typography } from '@mui/material';
+import Moon1 from '../../assets/moon_1.jpg'
+import Moon2 from '../../assets/moon_2.jpg'
+
 
 function Moon() {
 
+    const pics = { Moon1, Moon2 }
+    const [selected, setSelected] = useState(pics.Moon1)
+
     return (
     <>
-        <div className='moon-component'>
-            <Typography variant="h2" m={10}>
-                Welcome to the Moon!
-            </Typography>
-
-            <Box
-            sx={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                '& > :not(style)': {
-                ml: 10,
-                mr: 10,
-                textAlign: 'center',
-                p: 3
-                },
-            }}
-            >
-                <Paper> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </Paper>
-            </Box>
+        <div className='moon-component' style={{ display:'flex', justifyContent:'center', pb:50}}>
+            <Card sx={{ maxWidth: 450 }}>
+                <CardMedia
+                    sx={{ height: 450 }}
+                    image={selected}
+                    title="moon"
+                />
+                <CardContent>
+                    {/* <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                    </Typography> */}
+                    <Typography variant="h6" color="text.secondary">
+                    The moon, a distant, celestial companion, inspires the hope of shared adventures in the cosmos. It's as though it whispers secrets of far-off galaxies and uncharted realms, igniting a desire to explore the mysteries of the universe together. In its soft, romantic light, we find a cosmic canvas upon which our story is painted, an eternal masterpiece of togetherness and exploration.
+                    </Typography>
+                </CardContent>
+                <CardActions >
+                    <Button onClick={() => selected == pics.Moon1 ? setSelected(pics.Moon2) : setSelected(pics.Moon1)} size="large">Change</Button>
+                </CardActions>
+                </Card>
         </div>
     </>
     )
